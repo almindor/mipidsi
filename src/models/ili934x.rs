a3f0a4c05757c6245ba07f3d5174706b61181dfd
Juraj Michálek <juraj.michalek@gmail.com>
2023-05-23 09:21:22 +0200

    Bugfix/ili init (#2)
    * ili934x with initialization delays
    
    * doc: add infor about embedded-graphics-core 0.4.0 and initialization delays for ILI934x

--- src/models/ili934x.rs
+++ src/models/ili934x.rs
@@ -23,25 +23,29 @@ where
 {
     let madctl = SetAddressMode::from(options);
 
-    // Datasheet info - 8.2.2. Software Reset (01h): It will be necessary to wait 5msec before sending new command following software reset. The display module loads all display
-    // supplier factory default values to the registers during this 5msec
+    // 15.4:  It is necessary to wait 5msec after releasing RESX before sending commands.
+    // 8.2.2: It will be necessary to wait 5msec before sending new command following software reset.
     delay.delay_us(5_000);
 
-    dcs.write_command(ExitSleepMode)?; // turn off sleep
+    dcs.write_command(madctl)?;
+    dcs.write_raw(0xB4, &[0x0])?;
+    dcs.write_command(SetInvertMode(options.invert_colors))?;
+    dcs.write_command(SetPixelFormat::new(pixel_format))?;
 
-    // Datasheet info - 8.2.2. Software Reset (01h): If Software Reset is applied during Sleep Out mode, it will be
-    // necessary to wait 120msec before sending Sleep out command. Software Reset Command cannot be sent during Sleep Out
-    // sequence.
+    dcs.write_command(EnterNormalMode)?;
+
+    // 8.2.12: It will be necessary to wait 120msec after sending Sleep In command (when in Sleep Out mode)
+    //          before Sleep Out command can be sent.
+    // The reset might have implicitly called the Sleep In command if the controller is reinitialized.
     delay.delay_us(120_000);
 
-    dcs.write_command(madctl)?; // left -> right, bottom -> top RGB
-    dcs.write_raw(0xB4, &[0x0])?; //Inversion Control [00]
-    dcs.write_command(SetInvertMode(options.invert_colors))?; // set color inversion
-    dcs.write_command(SetPixelFormat::new(pixel_format))?; // pixel format
+    dcs.write_command(ExitSleepMode)?;
 
-    dcs.write_command(EnterNormalMode)?; // turn to normal mode
+    // 8.2.12: It takes 120msec to become Sleep Out mode after SLPOUT command issued.
+    // 13.2 Power ON Sequence: Delay should be 60ms + 80ms
+    delay.delay_us(140_000);
 
-    dcs.write_command(SetDisplayOn)?; // turn on display
+    dcs.write_command(SetDisplayOn)?;
 
     Ok(madctl)
 }
